01ed338baf0775c621f6b18caea96e56e4d25995
terut <terut.dev+github@gmail.com>
2011-10-03 20:45:41 +0900

    initial commit.

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+gem 'haml-rails'
+gem 'mongoid'
+gem 'bson_ext'
+
+group :development, :test do
+  gem 'ruby-debug19', :require => 'ruby-debug'
+  gem 'rspec-rails'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sknow::Application.config.secret_token = '3f5ca529b2e9389d1ccec65e7fd612077d2ef873a2019c107572e1727f5a7f6ca2544e1037f28c0892010b5b6f88d58bfaf6f824aacfeede2d245cba97f323fd'
